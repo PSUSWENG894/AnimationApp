7def9583150d90a4e35e070ee94d841c21b6c3bd
georgemgeorge <georgemgeorge@gmail.com>
2018-06-15 19:04:31 -0500

    Testing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,7 +17,7 @@
         name: 'Home',
         data() {
             return {
-                msg: 'Welcome to My Animation! App'
+                msg: 'Welcome to My Animation!'
             }
         },
         methods: {
