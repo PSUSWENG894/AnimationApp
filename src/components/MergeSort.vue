022acb6e1fb0aef96e9ac4e0138dd2affe2dd4c2
georgemgeorge <georgemgeorge@gmail.com>
2018-07-03 21:44:49 -0500

    Tweaking Animation and Unit Test Stuff.

--- src/components/MergeSort.vue
+++ src/components/MergeSort.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="hello">
         <h1>{{ msg }}</h1>
-        <button type="button" v-on:click=play()>Play</button>
+        <button type="button" v-if="loaded" v-on:click=play()>Play</button>
+        <button type="button" v-if="loaded" v-on:click=pause()>Pause</button>
+        <button type="button" v-if="loaded" v-on:click=restart()>Restart</button>
         <br><br>
         <div>
             <div class="box b1">
@@ -34,146 +36,150 @@
 
     export default {
         name: 'MergeSort',
-        data() {
+        data: function () {
+
             return {
-                msg: 'Merge Sort'
+                msg: 'Merge Sort',
+                boxTimeline: null,
+                loaded: false,
             }
         },
-        methods: {
-            play: function () {
-                let loop = false;
-                let easing = 'linear';
-                let direction = 'alternate';
 
-                // anime({
-                //     targets: '.b1',
-                //     translateX: 0,
-                //     translateY: 5,
-                //     easing,
-                //     loop,
-                //     direction,
-                //     background: [
-                //         {value: '#ffec42'},
-                //         {value: '#ff0038'}
-                //     ]
-                // });
-                let boxTimeline = anime.timeline({
-                    loop,
-                    direction
-                });
+        mounted: function () {
+            let loop = false;
+            let easing = 'linear';
+            let direction = 'alternate';
 
-                let xTrans = [{value: 100},
-                    {value: 200},
-                    {value: 300},
-                    {value: 400},
-                    {value: 500},
-                    {value: 600}];
-                let bgcolor = [{value: '#ffffff'},
-                    {value: '#ffffff'},
-                    {value: '#ffffff'},
-                    {value: '#00ff00'},
-                    {value: '#00ff00'},
-                    {value: '#00ff00'},
-                    {value: '#00ff00'}];
+            this.boxTimeline = anime.timeline({
+                loop,
+                direction,
+                autoplay: false,
+            });
 
-                let duration = 16000;
+            let xTrans = [{value: 100},
+                {value: 200},
+                {value: 300},
+                {value: 400},
+                {value: 500},
+                {value: 600}];
+            let bgcolor = [{value: '#ffffff'},
+                {value: '#ffffff'},
+                {value: '#ffffff'},
+                {value: '#00ff00'},
+                {value: '#00ff00'},
+                {value: '#00ff00'},
+                {value: '#00ff00'}];
 
-                boxTimeline
-                    .add({
-                        targets: '.b1',
-                        translateY: [{value: '-20'},
-                            {value: '-40'},
-                            {value: '-80'},
-                            {value: 0},
-                            {value: 85},
-                            {value: 220}],
-                        translateX: xTrans,
-                        background: bgcolor,
-                        duration: duration,
-                        offset: 0,
-                        easing
-                    }).add({
-                    targets: '.b2',
+            let duration = 16000;
+
+            this.boxTimeline
+                .add({
+                    targets: '.b1',
                     translateY: [{value: '-20'},
                         {value: '-40'},
-                        {value: '-60'},
-                        {value: '-110'},
-                        {value: '-25'},
-                        {value: 110}],
-                    translateX: xTrans,
-                    background: bgcolor,
-                    duration: duration,
-                    offset: 0,
-                    easing
-                }).add({
-                    targets: '.b3',
-                    translateY: [{value: '-20'},
-                        {value: '-20'},
-                        {value: '-40'},
-                        {value: 20},
-                        {value: 30},
-                        {value: 165}],
-                    translateX: xTrans,
-                    background: bgcolor,
-                    duration: duration,
-                    offset: 0,
-                    easing
-                }).add({
-                    targets: '.b4',
-                    translateY: [{value: '-20'},
-                        {value: '-20'},
-                        {value: '-90'},
-                        {value: '-90'},
-                        {value: '-190'},
-                        {value: '-165'}],
-                    translateX: xTrans,
-                    background: bgcolor,
-                    duration: duration,
-                    offset: 0,
-                    easing
-                }).add({
-                    targets: '.b5',
-                    translateY: [{value: 20},
-                        {value: 20},
+                        {value: '-80'},
                         {value: 0},
-                        {value: 20},
-                        {value: 20},
-                        {value: '-165'},],
+                        {value: 85},
+                        {value: 220}],
                     translateX: xTrans,
                     background: bgcolor,
                     duration: duration,
                     offset: 0,
                     easing
                 }).add({
-                    targets: '.b6',
-                    translateY: [{value: 20},
-                        {value: 20},
-                        {value: 20},
-                        {value: 20},
-                        {value: 75},
-                        {value: 55}],
-                    translateX: xTrans,
-                    background: bgcolor,
-                    duration: duration,
-                    offset: 0,
-                    easing
-                }).add({
-                    targets: '.b7',
-                    translateY: [{value: 20},
-                        {value: 40},
-                        {value: 40},
-                        {value: 40},
-                        {value: '-35'},
-                        {value: '-220'}],
-                    translateX: xTrans,
-                    background: bgcolor,
-                    duration: duration,
-                    offset: 0,
-                    easing
-                })
+                targets: '.b2',
+                translateY: [{value: '-20'},
+                    {value: '-40'},
+                    {value: '-60'},
+                    {value: '-110'},
+                    {value: '-25'},
+                    {value: 110}],
+                translateX: xTrans,
+                background: bgcolor,
+                duration: duration,
+                offset: 0,
+                easing
+            }).add({
+                targets: '.b3',
+                translateY: [{value: '-20'},
+                    {value: '-20'},
+                    {value: '-40'},
+                    {value: 20},
+                    {value: 30},
+                    {value: 165}],
+                translateX: xTrans,
+                background: bgcolor,
+                duration: duration,
+                offset: 0,
+                easing
+            }).add({
+                targets: '.b4',
+                translateY: [{value: '-20'},
+                    {value: '-20'},
+                    {value: '-90'},
+                    {value: '-90'},
+                    {value: '-190'},
+                    {value: '-165'}],
+                translateX: xTrans,
+                background: bgcolor,
+                duration: duration,
+                offset: 0,
+                easing
+            }).add({
+                targets: '.b5',
+                translateY: [{value: 20},
+                    {value: 20},
+                    {value: 0},
+                    {value: 20},
+                    {value: 20},
+                    {value: '-165'},],
+                translateX: xTrans,
+                background: bgcolor,
+                duration: duration,
+                offset: 0,
+                easing
+            }).add({
+                targets: '.b6',
+                translateY: [{value: 20},
+                    {value: 20},
+                    {value: 20},
+                    {value: 20},
+                    {value: 75},
+                    {value: 55}],
+                translateX: xTrans,
+                background: bgcolor,
+                duration: duration,
+                offset: 0,
+                easing
+            }).add({
+                targets: '.b7',
+                translateY: [{value: 20},
+                    {value: 40},
+                    {value: 40},
+                    {value: 40},
+                    {value: '-35'},
+                    {value: '-220'}],
+                translateX: xTrans,
+                background: bgcolor,
+                duration: duration,
+                offset: 0,
+                easing
+            })
+            this.loaded = true;
+        },
+        methods: {
+            play() {
+                this.boxTimeline.play();
+            },
+            pause() {
+                this.boxTimeline.pause();
+            },
+            restart() {
+                this.boxTimeline.restart();
+                this.boxTimeline.pause();
             }
-
         }
+
     }
 </script>
 
---
+++ test/unit/specs/MergeSort.spec.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import MergeSort from '@/components/MergeSort'
+
+describe('MergeSort.vue', () => {
+    it('all elements should be present', () => {
+        const Constructor = Vue.extend(MergeSort)
+        const vm = new Constructor().$mount()
+
+        let b1 = vm.$el.querySelector('.b1');
+        let b2 = vm.$el.querySelector('.b2');
+        let b3 = vm.$el.querySelector('.b3');
+        let b4 = vm.$el.querySelector('.b4');
+        let b5 = vm.$el.querySelector('.b5');
+        let b6 = vm.$el.querySelector('.b6');
+        let b7 = vm.$el.querySelector('.b7');
+
+
+        expect(b1).to.not.be.null;
+        expect(b2).to.not.be.null;
+        expect(b3).to.not.be.null;
+        expect(b4).to.not.be.null;
+        expect(b5).to.not.be.null;
+        expect(b6).to.not.be.null;
+        expect(b7).to.not.be.null;
+
+
+        //   expect(vm.$el.querySelector('.hello h1').attributes)
+        //     .to.equal('Welcome to Your Vue.js App')
+    })
+});
