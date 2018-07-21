af24324438cf21cec89318d2a0acd9c08ef54f8e
georgemgeorge <georgemgeorge3@gmail.com>
2018-07-21 18:04:41 -0500

    Layout Work

--- src/components/MatrixMultiply.vue
+++ src/components/MatrixMultiply.vue
@@ -1,6 +1,120 @@
 <template>
     <div>
         <ControlButtons v-bind:play="play" v-bind:pause="pause" v-bind:restart="restart"/>
+        <br><br><br><br><br><br><br><br>
+        <div>
+            <md-layout>
+                <md-layout-item class="box m1 r1 b1">
+                    1
+                </md-layout-item>
+                <md-layout-item class="box m1 r1 b2">
+                    2
+                </md-layout-item>
+                <md-layout-item class="box m1 r1 b3">
+                    3
+                </md-layout-item>
+                <md-layout-item class="blank">
+                    &nbsp;
+                </md-layout-item>
+                <md-layout-item class="box m2 r1 b1">
+                    1
+                </md-layout-item>
+                <md-layout-item class="box m2 r1 b2">
+                    2
+                </md-layout-item>
+                <md-layout-item class="box m2 r1 b3">
+                    3
+                </md-layout-item>
+                <md-layout-item class="blank">
+                    &nbsp;
+                </md-layout-item>
+                <md-layout-item class="box m3 r1 b1">
+                    1
+                </md-layout-item>
+                <md-layout-item class="box m3 r1 b2">
+                    2
+                </md-layout-item>
+                <md-layout-item class="box m3 r1 b3">
+                    3
+                </md-layout-item>
+            </md-layout>
+        </div>
+        <br><br>
+        <div>
+            <md-layout>
+                <md-layout-item class="box m1 r2 b1">
+                    4
+                </md-layout-item>
+                <md-layout-item class="box m1 r2 b2">
+                    5
+                </md-layout-item>
+                <md-layout-item class="box m1 r2 b3">
+                    6
+                </md-layout-item>
+                <md-layout-item class="multiply">
+                    *
+                </md-layout-item>
+                <md-layout-item class="box m2 r2 b1">
+                    4
+                </md-layout-item>
+                <md-layout-item class="box m2 r2 b2">
+                    5
+                </md-layout-item>
+                <md-layout-item class="box m2 r2 b3">
+                    6
+                </md-layout-item>
+                <md-layout-item class="equals">
+                    =
+                </md-layout-item>
+                <md-layout-item class="box m3 r2 b1">
+                    4
+                </md-layout-item>
+                <md-layout-item class="box m3 r2 b2">
+                    5
+                </md-layout-item>
+                <md-layout-item class="box m3 r2 b3">
+                    6
+                </md-layout-item>
+            </md-layout>
+        </div>
+        <br><br>
+        <div>
+            <md-layout>
+                <md-layout-item class="box m1 r3 b1">
+                    7
+                </md-layout-item>
+                <md-layout-item class="box m1 r3 b2">
+                    8
+                </md-layout-item>
+                <md-layout-item class="box m1 r3 b3">
+                    9
+                </md-layout-item>
+                <md-layout-item class="blank">
+                    &nbsp;
+                </md-layout-item>
+                <md-layout-item class="box m2 r3 b1">
+                    7
+                </md-layout-item>
+                <md-layout-item class="box m2 r3 b2">
+                    8
+                </md-layout-item>
+                <md-layout-item class="box m2 r3 b3">
+                    9
+                </md-layout-item>
+                <md-layout-item class="blank">
+                    &nbsp;
+                </md-layout-item>
+                <md-layout-item class="box m3 r3 b1">
+                    7
+                </md-layout-item>
+                <md-layout-item class="box m3 r3 b2">
+                    8
+                </md-layout-item>
+                <md-layout-item class="box m3 r3 b3">
+                    9
+                </md-layout-item>
+            </md-layout>
+        </div>
     </div>
 </template>
 
@@ -32,5 +146,63 @@
 </script>
 
 <style scoped>
+
+
+    .box {
+        width: 50px;
+        height: 50px;
+        border: 10px solid green;
+        padding: 10px;
+        margin: 5px;
+        background-color: white;
+    }
+
+    .blank {
+        text-align: center;
+        padding: 40px;
+        color: white;
+        font-size: 50px;
+    }
+
+    .multiply {
+        text-align: center;
+        padding: 35px;
+        color: white;
+        font-size: 50px;
+    }
+
+    .equals {
+        text-align: center;
+        padding: 30px;
+        color: white;
+        font-size: 50px;
+    }
+
+    .m1 {
+    }
+
+    .m2 {
+    }
+
+    .m3 {
+    }
+
+    .r1 {
+    }
+
+    .r2 {
+    }
+
+    .r3 {
+    }
+
+    .b1 {
+    }
+
+    .b2 {
+    }
+
+    .b3 {
+    }
 
 </style>
